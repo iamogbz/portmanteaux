5684d86a4a0c6384e1c9508fea6775acee4f8766
iamogbz <iamogbz+github@gmail.com>
2021-07-11 22:47:25 -0400

    3 files modified

--- src/components/Portmanteaux/index.tsx
+++ src/components/Portmanteaux/index.tsx
@@ -1,28 +1,27 @@
-import * as React from "react";
-import styled from "styled-components";
+import * as React from 'react';
+import styled from 'styled-components';
 
-import { Colors } from "../../constants/styles";
+import { Colors } from '../../constants/styles';
 import { usePortmanteauxList } from './usePortmanteauxList';
 
-
 const Word = styled.span`
-    border: solid 1px ${Colors.FOREGROUND};
-    border-radius: 2px;
-    margin: 2px;
-    padding: 4px;
+  border: solid 1px ${Colors.FOREGROUND};
+  border-radius: 2px;
+  margin: 2px;
+  padding: 4px;
 `;
 
 export function Portmanteaux(props: { filterText?: string }): JSX.Element {
-    const wordList = usePortmanteauxList();
-    const matchingWordList = React.useMemo(() => {
-        return wordList.filter((word) => word.includes(props.filterText ?? ""));
-    }, [props.filterText]);
+  const wordList = usePortmanteauxList();
+  const matchingWordList = React.useMemo(() => {
+    return wordList.filter((word) => word.includes(props.filterText ?? ''));
+  }, [props.filterText]);
 
-    return (
-        <div>
-            {matchingWordList.map((word) => (
-                <Word key={word}>{word}</Word>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {matchingWordList.map((word) => (
+        <Word key={word}>{word}</Word>
+      ))}
+    </div>
+  );
 }
