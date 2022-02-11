1a77c5c62a71b86b5d447751a928c833463530bf
iamogbz <iamogbz+github@gmail.com>
2022-02-11 02:07:26 -0500

    1 file modified

--- src/components/Portmanteaux/utils/object.ts
+++ src/components/Portmanteaux/utils/object.ts
@@ -7,21 +7,21 @@ type CollectionToObject<T> = T extends Map<Key, infer V>
   : T;
 
 export function collectionToObject<V>(
-  c: Map<Key, V> | Set<V> | unknown
+  c: Map<Key, V> | Set<V> | V
 ): CollectionToObject<typeof c> {
   if (c instanceof Map) {
     const o = {} as CollectionToObject<Map<Key, V>>;
     c.forEach((v: V, k: Key) => {
-      o[k] = collectionToObject(v);
+      o[k] = collectionToObject(v) as CollectionToObject<V>;
     });
     return o;
   } else if (c instanceof Set) {
     const l = [] as CollectionToObject<Set<V>>;
     c.forEach((v: V) => {
-      l.push(collectionToObject(v));
+      l.push(collectionToObject(v) as CollectionToObject<V>);
     });
     return l;
   } else {
-    return c;
+    return c as CollectionToObject<V>;
   }
 }
